b47b3095f5f60956570f9d627c73ea30a62332fe
moussa32 <moussaebrahem32@gmail.com>
2021-02-02 17:25:51 +0200

    Created Navbar and Home components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>Title</h1>
+        <Home/>
       </div>
     </div>
   );
