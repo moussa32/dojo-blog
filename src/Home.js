a28b5e4d707b263f1e826e4f5076cc5f916eb754
moussa32 <moussaebrahem32@gmail.com>
2021-02-02 17:54:10 +0200

    Adding events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+
+    const handelClick = (eventObject) => {
+        console.log('Hello');
+        console.log(eventObject);
+    }
+    const handelClickWithArgument = (name, e) => {
+        console.log(name);
+        console.log(e.target);
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handelClick}>Click here</button>
+            <button onClick={(e) => handelClickWithArgument('Moussa', e)}>Click With argument</button>
         </div>
     );
 }
