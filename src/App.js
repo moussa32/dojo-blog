1819f41ca50e6ff32f7a7a3177d197ae3c3d2c70
moussa32 <moussaebrahem32@gmail.com>
2021-02-02 17:15:26 +0200

    clean up the old App component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import './App.css';
 
 function App() {
-  const title = 'Welcome Moussa';
-  const likes = 50;
-  // const body = { arm: 'Yeah', age : 50}
-  const link = 'https://www.google.com';
-
   return (
     <div className="App">
       <div className="content">
-        <h1>{ title }</h1>
-        <p>Liked { likes } time</p>
-
-        {/* <p>{ body }</p> */}
-
-        <p>{ 10 }</p>
-        <p>{"Hello people"}</p>
-        <p>{[0,2,5,72,1,4,6]}</p>
-        <p>{Math.random() * 10}</p>
-
-        <a href={link}>Google site.</a>
+        <h1>Title</h1>
       </div>
     </div>
   );
