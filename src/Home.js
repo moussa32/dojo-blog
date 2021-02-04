ca62bc1554d1d57b595f0adf750582d10a849ba1
moussa32 <moussaebrahem32@gmail.com>
2021-02-04 12:55:13 +0200

    adding useState

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,21 @@
-const Home = () => {
+import { useState } from 'react';
 
-    const handelClick = (eventObject) => {
-        console.log('Hello');
-        console.log(eventObject);
-    }
-    const handelClickWithArgument = (name, e) => {
-        console.log(name);
-        console.log(e.target);
+const Home = () => {
+    //let name = 'mario';
+    const [name, setName] = useState('Mario');
+    const [age, setAge] = useState(25);
+
+    
+    const handelClick = () => {
+        setName('luigi');
+        setAge(55);
     }
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name } is { age } years old.</p>
             <button onClick={handelClick}>Click here</button>
-            <button onClick={(e) => handelClickWithArgument('Moussa', e)}>Click With argument</button>
         </div>
     );
 }
