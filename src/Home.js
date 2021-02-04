e98acd9573118ba053a5bc31ea529c6e16d578f8
moussa32 <moussaebrahem32@gmail.com>
2021-02-04 13:10:06 +0200

    reset

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,9 @@
 import { useState } from 'react';
 
 const Home = () => {
-    //let name = 'mario';
-    const [name, setName] = useState('Mario');
-    const [age, setAge] = useState(25);
-
-    
-    const handelClick = () => {
-        setName('luigi');
-        setAge(55);
-    }
-
     return ( 
         <div className="home">
             <h2>Homepage</h2>
-            <p>{ name } is { age } years old.</p>
-            <button onClick={handelClick}>Click here</button>
         </div>
     );
 }
